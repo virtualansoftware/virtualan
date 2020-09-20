a3c988da61efc3f5e19c0b1eacfb030ff081ad40
Elan Thangamani <elans3.java@gmail.com>
2020-09-20 01:16:38 -0500

    Added standard template

--- modules/virtualization/src/main/java/io/virtualan/core/soap/SoapEndpointCodeGenerator.java
+++ modules/virtualization/src/main/java/io/virtualan/core/soap/SoapEndpointCodeGenerator.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2020 Virtualan Contributors (https://virtualan.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.virtualan.core.soap;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -44,6 +58,13 @@ import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.SystemPropertyUtils;
+
+/**
+ * This is Soap Virtual Service .
+ *
+ * @author  Elan Thangamani
+ *
+ **/
 
 @Configuration
 @Component
