e67c2eb6993616a7c0c9aa0a9956b19515f4bf78
Elan Thangamani <elans3.java@gmail.com>
2020-10-21 07:49:12 -0500

    Added import

--- modules/virtualization/src/main/java/io/virtualan/aop/ApiVirtualAspect.java
+++ modules/virtualization/src/main/java/io/virtualan/aop/ApiVirtualAspect.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Virtualan Contributors (https://virtualan.io)
+ * Copyright 2020 Virtualan Contributors (https://virtualan.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -26,6 +26,8 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.json.JSONException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.CookieParam;
