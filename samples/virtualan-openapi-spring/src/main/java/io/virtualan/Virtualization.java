7408b1f80a65c2af4a5fcd40f482c109a23bc2dc
Elan Thangamani <elans3.java@gmail.com>
2021-02-07 23:41:27 -0600

    Virtualization application

---
+++ samples/virtualan-openapi-spring/src/main/java/io/virtualan/Virtualization.java
@@ -0,0 +1,34 @@
+package io.virtualan;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"io.virtualan", "io.virtualan.configuration"})
+public class Virtualization implements CommandLineRunner {
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+            throw new ExitException();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(Virtualization.class).run(args);
+    }
+
+    class ExitException extends RuntimeException implements ExitCodeGenerator {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int getExitCode() {
+            return 10;
+        }
+
+    }
+
+}
