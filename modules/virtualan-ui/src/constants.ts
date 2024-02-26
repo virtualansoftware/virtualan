db94c490ddb7ba3ac480b945a4ebf81097065dfe
Elan Thangamani <elans3.java@gmail.com>
2024-02-25 19:40:58 -0600

    removed the hard coded value

--- modules/virtualan-ui/src/constants.ts
+++ modules/virtualan-ui/src/constants.ts
@@ -1,8 +1,8 @@
-export const API_GET_ENDPOINT_ADD = ENV_URL + "/virtualservices/load";
-export const API_GET_ENDPOINT_LOAD = ENV_URL + "/virtualservices";
-export const API_DELETE_ENDPOINT = ENV_URL + "/virtualservices";
-export const API_POST_ENDPOINT = ENV_URL + "/virtualservices";
-export const API_GET_CATALOGS = ENV_URL + "/api-catalogs";
+export const API_GET_ENDPOINT_ADD = "/virtualservices/load";
+export const API_GET_ENDPOINT_LOAD = "/virtualservices";
+export const API_DELETE_ENDPOINT = "/virtualservices";
+export const API_POST_ENDPOINT = "/virtualservices";
+export const API_GET_CATALOGS = "/api-catalogs";
 export const VERSION = "v3.0.0";
 
 
