0c30ce404b718b46adbf2b8220aa8688c50c6d09
elanj <elans3.java@gmail.com>
2024-10-19 23:20:21 -0500

    Code upgraded with 3.x spring and Java17

--- modules/virtualization/src/main/java/io/virtualan/autoconfig/StaticResourceConfiguration.java
+++ modules/virtualization/src/main/java/io/virtualan/autoconfig/StaticResourceConfiguration.java
@@ -4,14 +4,21 @@ import io.virtualan.core.util.VirtualanConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 
 @Configuration
+//@EnableWebMvc
 @AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
 public class StaticResourceConfiguration implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/virtualan-ui").setViewName("static/index.html");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
